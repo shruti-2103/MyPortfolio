5fafe9cb73bd73fa00be82e262cadd2d4ec660ae
shruti chourasiya <shruti210300@gmail.com>
2025-12-03 23:32:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
